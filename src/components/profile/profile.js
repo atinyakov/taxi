24b2fb37b88873007415afb3627d7f66ccaf1492
atinyakov <akoff16@gmail.com>
2019-10-30 00:06:05 +0300

    'working_version'

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,12 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 // import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
-import Button from '@material-ui/core/Button';
-
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -22,37 +20,6 @@ const useStyles = makeStyles(theme => ({
 
 const Profile = () => {
     const classes = useStyles();
-    
-    // const [card, setCard] = useState({
-    //     creditCard: {
-    //         cardHolder: '',
-    //         number: '',
-    //         csv: '',
-    //         expiration: ''
-    //     }
-    // })
-    const [holder, setHolder]  = useState('');
-    const [number, setNumber]  = useState('');
-    const [expiration, setExpiration]  = useState('');
-    const [csv, setCSV]  = useState('');
-
-
-
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('here');
-        // setCard({
-
-        // })
-    }
-
-    const handleInputChange = (e) => {
-        e.preventDefault();
-        // evt.target.name
-    }
-
 
     return (
         <Modal
@@ -62,38 +29,22 @@ const Profile = () => {
           // onClose={handleClose}
         >
             <div className={classes.paper}>
-                <form onSubmit = { (e) => this.handleSubmit(e) } >
+
                 <Typography variant="h5" gutterBottom>
                     Profile
                 </Typography> 
                     <Grid container spacing={3}>
                     <Grid item xs={12} md={6}>
-                    <TextField  onChange={this.handleInputChange}
-                                required
-                                name="holder"
-                                id="cardName"
-                                label="Name on card"
-                                fullWidth />
+                    <TextField required id="cardName" label="Name on card" fullWidth />
                     </Grid>
                     <Grid item xs={12} md={6}>
-                    <TextField onChange={this.handleInputChange}
-                                name="number"
-                                required
-                                id="cardNumber"
-                                label="Card number"
-                                fullWidth />
+                    <TextField required id="cardNumber" label="Card number" fullWidth />
                     </Grid>
                     <Grid item xs={12} md={6}>
-                    <TextField  onChange={handleInputChange}
-                                name="csv"
-                                required
-                                id="expDate" 
-                                label="Expiry date" 
-                                fullWidth />
+                    <TextField required id="expDate" label="Expiry date" fullWidth />
                     </Grid>
                     <Grid item xs={12} md={6}>
                     <TextField
-                        onChange={handleInputChange}
                         required
                         id="cvv"
                         label="CVV"
@@ -101,18 +52,7 @@ const Profile = () => {
                         fullWidth
                     />
                     </Grid>
-                    <Grid item xs={12} md={6}>
-                        <Button
-
-                            type="submit"
-                            // onClick={handleNext}
-                            className={classes.button}
-                        >
-                            Save
-                        </Button>
-                    </Grid>
                 </Grid>
-                </form>
             </div>
         </Modal>
     )
