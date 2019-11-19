01e90957042c469ff692170c763fcc2844345a5a
atinyakov <akoff16@gmail.com>
2019-11-20 00:01:06 +0300

    add sagas

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './components/App'
 import 'typeface-roboto';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, compose, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import { taxiApp } from './reducer'
-import { loginMiddleWare } from './middleware'
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { taxiApp } from './reducer';
+import { loginMiddleWare } from './middleware';
+import createSagaMiddleware from 'redux-saga';
+import { dataSaga } from './sagas'
 
 let prevState = localStorage.getItem('user');
 let userData = {...JSON.parse(prevState)};
 
-let store = createStore(taxiApp, userData, compose(
+const sagaMiddleware = createSagaMiddleware();
+sagaMiddleware.run(dataSaga);
+
+
+let store = createStore(taxiApp, userData
+    , compose(
     applyMiddleware(loginMiddleWare),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
