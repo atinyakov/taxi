62d1b65a9c2722975ddc83b60bb288564a42e592
atinyakov <akoff16@gmail.com>
2019-11-20 02:10:57 +0300

    init sagas

--- src/index.js
+++ src/index.js
@@ -6,24 +6,24 @@ import { BrowserRouter } from 'react-router-dom';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { taxiApp } from './reducer';
-import { loginMiddleWare } from './middleware';
+// import { loginMiddleWare } from './middleware';
 import createSagaMiddleware from 'redux-saga';
 import { dataSaga } from './sagas'
 
 let prevState = localStorage.getItem('user');
-let userData = {...JSON.parse(prevState)};
+let userData = { ...JSON.parse(prevState) };
 
 const sagaMiddleware = createSagaMiddleware();
-sagaMiddleware.run(dataSaga);
 
 
 let store = createStore(taxiApp, userData
     , compose(
-    applyMiddleware(loginMiddleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(sagaMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 )
 
+sagaMiddleware.run(dataSaga);
 ReactDOM.render(
     <Provider store={store} >
         <BrowserRouter>
--- src/sagas.js
+++ src/sagas.js
@@ -1,3 +1,64 @@
+import { call, all, takeLatest, takeEvery } from 'redux-saga/effects';
+import { login, logout, loginData, signUpData, cardData } from './action';
+
+let user = {
+    userDataHandler: {
+        email: '',
+        password: ''
+    },
+    loginHandler: {
+        isLoggedIn: true
+    },
+    card: {
+        expiryDate: '',
+        cvc: ''
+    }
+
+};
+
+function postData(email, password) {
+    user = {
+        ...user,
+        userDataHandler: {...user.userDataHandler}, email, password
+    }
+
+    fetch("https://loft-taxi.glitch.me/auth", {
+        body: JSON.stringify(user),
+        method: 'POST',
+        headers: {
+            "content-Type": "application/json"
+        }
+    }).then(responce => {
+        responce.json();
+    });
+}
+
+export function* handleUpload() {
+    yield takeEvery('LOGIN_DATA', function* () {
+        const result = yield call(postData);
+        console.log(result)
+    });
+}
+
+// export function* dataSaga() {
+//     console.log('Hello Sagas!')
+// }
+
+// function* watchRegister() {
+//     yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+// }
+
+// export function* postData(action) {
+//     try {
+//         const data = yield call(Api.fetchUser, action.payload.url)
+//         yield put({ type: "FETCH_SUCCEEDED", data })
+//     } catch (error) {
+//         yield put({ type: "FETCH_FAILED", error })
+//     }
+// }
+
 export function* dataSaga() {
-    console.log('Hello Sagas!')
+    yield all([
+        handleUpload(),
+    ])
 }
