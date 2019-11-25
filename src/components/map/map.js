6880b32f7d1acb497d26e9718d1bc7846d6ae977
atinyakov <akoff16@gmail.com>
2019-11-26 01:10:10 +0300

    id route issue

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,130 +1,87 @@
-import "mapbox-gl/dist/mapbox-gl.css";
-import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import React, { Component } from "react";
-import MapGL from "react-map-gl";
-import nanoid from "nanoid/non-secure";
-import DeckGL, { GeoJsonLayer } from "deck.gl";
-import Geocoder from "react-map-gl-geocoder";
+// import MapGL from "react-map-gl";
+// import nanoid from "nanoid/non-secure";
+// import DeckGL, { GeoJsonLayer } from "deck.gl";
+// import Geocoder from "react-map-gl-geocoder";
 import Destination from "../Destination";
 import { connect } from "react-redux";
-const token = `pk.eyJ1IjoiYWtvZmYiLCJhIjoiY2syNTd3NHRvMTQzcTNtbXp4ZnAxNWs5YyJ9.ELtn_IIvz8p_0R6ujfH8Hw`;
-// const token = process.env.REACT_APP_TOKEN
+import mapboxgl from "mapbox-gl";
+import "./style.css";
+mapboxgl.accessToken = `pk.eyJ1IjoiYWtvZmYiLCJhIjoiY2syNTd3NHRvMTQzcTNtbXp4ZnAxNWs5YyJ9.ELtn_IIvz8p_0R6ujfH8Hw`;
 
-export class Map extends Component {
-  state = {
-    viewport: {
-      latitude: 0,
-      longitude: 0,
-      zoom: 1
-    },
-    id: nanoid(),
-    searchResultLayer: null
-  };
+class Map extends Component {
+  // Code from the next few steps will go here
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 2
+    };
+  }
 
-  mapRef = React.createRef();
+  map = '';
 
-  handleViewportChange = viewport => {
-    this.setState({
-      viewport: { ...this.state.viewport, ...viewport }
+  componentDidMount() {
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
     });
-  };
-  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
-  handleGeocoderViewportChange = viewport => {
-    const geocoderDefaultOverrides = { transitionDuration: 1000 };
 
-    return this.handleViewportChange({
-      ...viewport,
-      ...geocoderDefaultOverrides
+    this.map.on("move", () => {
+      this.setState({
+        lng: this.map.getCenter().lng.toFixed(4),
+        lat: this.map.getCenter().lat.toFixed(4),
+        zoom: this.map.getZoom().toFixed(2)
+      });
     });
-  };
-
-  handleOnResult = event => {
-    this.setState({
-      searchResultLayer: new GeoJsonLayer({
-        id: "search-result",
-        data: event.result.geometry,
-        getFillColor: [255, 0, 0, 128],
-        getRadius: 1000,
-        pointRadiusMinPixels: 10,
-        pointRadiusMaxPixels: 10
-      })
-    });
-  };
-
-  drawRoute = (map, coordinates) => {
-    map.flyTo({
-      center: coordinates[0],
-      zoom: 15
-    });
-    map.addLayer({
-      id: "route",
-      type: "line",
-      source: {
-        type: "geojson",
-        data: {
-          type: "Feature",
-          properties: {},
-          geometry: {
-            type: "LineString",
-            coordinates
-          }
-        }
-      },
-      layout: {
-        "line-join": "round",
-        "line-cap": "round"
-      },
-      paint: {
-        "line-color": "#ffc617",
-        "line-width": 8
-      }
-    });
-  };
-  
+  }
 
   componentDidUpdate() {
-    this.drawRoute(this.mapRef, this.props.coordinates)
+    drawRoute(this.map, this.props.coordinates)
   }
 
   render() {
-    const { id, viewport, searchResultLayer } = this.state;
-
     return (
-      <div style={{ height: "100vh" }}>
-        <div
-          style={{
-            textAlign: "center",
-            fontSize: "25px",
-            fontWeight: "bolder"
-          }}
-        >
-          Use the search bar to find a location or click <a href='/'>here</a> to
-          find your location
-          <Destination />
-        </div>
-        <MapGL
-          ref={this.mapRef}
-          {...viewport}
-          mapStyle='mapbox://styles/mapbox/streets-v9'
-          width='100%'
-          height='90%'
-          onViewportChange={this.handleViewportChange}
-          mapboxApiAccessToken={token}
-        >
-          <Geocoder
-            key={id}
-            mapRef={this.mapRef}
-            onResult={this.handleOnResult}
-            onViewportChange={this.handleGeocoderViewportChange}
-            mapboxApiAccessToken={token}
-            position='top-left'
-          />
-        </MapGL>
-        <DeckGL {...viewport} layers={[searchResultLayer]} />
+      <div>
+        <Destination />
+        <div ref={el => (this.mapContainer = el)} className='mapContainer' />
       </div>
     );
   }
 }
+
+export const drawRoute = (map, coordinates) => {
+  map.flyTo({
+    center: coordinates[0],
+    zoom: 15
+  });
+  map.addLayer({
+    id: "route",
+    type: "line",
+    source: {
+      type: "geojson",
+      data: {
+        type: "Feature",
+        properties: {},
+        geometry: {
+          type: "LineString",
+          coordinates
+        }
+      }
+    },
+    layout: {
+      "line-join": "round",
+      "line-cap": "round"
+    },
+    paint: {
+      "line-color": "#ffc617",
+      "line-width": 8
+    }
+  });
+};
 
 // export default Map;
 
@@ -133,6 +90,5 @@ const mapStateToProps = state => {
     coordinates: state.loginHandler.route
   };
 };
-
 
 export default connect(mapStateToProps)(Map);
--- src/index.js
+++ src/index.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App'
-import 'typeface-roboto';
-import { BrowserRouter } from 'react-router-dom';
-import { createStore, compose, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import { taxiApp } from './reducer';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "typeface-roboto";
+import { BrowserRouter } from "react-router-dom";
+import { createStore, compose, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { taxiApp } from "./reducer";
 // import { loginMiddleWare } from './middleware';
-import createSagaMiddleware from 'redux-saga';
-import { dataSaga } from './sagas'
+import createSagaMiddleware from "redux-saga";
+import { dataSaga } from "./sagas";
+import { theme } from "loft-taxi-mui-theme";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 
-let prevState = localStorage.getItem('user');
+let prevState = localStorage.getItem("user");
 let userData = { ...JSON.parse(prevState) };
 
 const sagaMiddleware = createSagaMiddleware();
 
-
-let store = createStore(taxiApp, userData
-    , compose(
-        applyMiddleware(sagaMiddleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-)
+let store = createStore(
+  taxiApp,
+  userData,
+  compose(
+    applyMiddleware(sagaMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 sagaMiddleware.run(dataSaga);
 ReactDOM.render(
-    <Provider store={store} >
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root'));
+  <MuiThemeProvider theme={theme}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>,
+  document.getElementById("root")
+);
